b886fe49835771aa4becf5a4c09b0ffe4bb37028
arnab9593 <arnabpal679@gmail.com>
2023-01-23 21:18:01 +0530

    release 1

---
+++ react-weather-app/src/App.js
@@ -0,0 +1,14 @@
+import TopButtons from "./Components/TopButtons";
+
+function App() {
+  return (
+    <>
+      <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+        <TopButtons />
+      </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ react-weather-app/src/Components/TopButtons.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function TopButtons() {
+
+    const cities = [
+        {
+            id: 1,
+            title: 'SanFransisco'
+        },
+        {
+            id: 2,
+            title: 'London'
+        },
+        {
+            id: 3,
+            title: 'Toronto'
+        },
+        {
+            id: 4,
+            title: 'Berlin'
+        }
+    ]
+
+    return (
+        <div >
+
+        </div>
+    )
+}
+
+export default TopButtons
