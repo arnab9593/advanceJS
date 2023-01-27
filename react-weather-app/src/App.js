8e136f35fee89cc715fd14487b650e3b073ed31f
arnab9593 <arnabpal679@gmail.com>
2023-01-28 00:39:22 +0530

    release 12

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,17 +1,19 @@
 // import TopButtons from "./Components/TopButtons";
 // import Inputs from "./Components/Inputs";
-import TimeAndLocation from "./Components/TimeAndLocation";
-import TemperatureDetails from "./Components/TemperatureDetails";
-import Forecast from "./Components/Forecast";
+// import TimeAndLocation from "./Components/TimeAndLocation";
+// import TemperatureDetails from "./Components/TemperatureDetails";
+// import Forecast from "./Components/Forecast";
 import getFormatedData from "./Services/weatherApi";
 import { useEffect, useState } from "react";
 import { Suspense, lazy } from "react";
 
-const Inputs = lazy(() => import("./Components/Inputs"));
 const TopButtons = lazy(() => import("./Components/TopButtons"));
+const Inputs = lazy(() => import("./Components/Inputs"));
+const TimeAndLocation = lazy(() => import("./Components/TimeAndLocation"));
+const TemperatureDetails = lazy(() => import("./Components/TemperatureDetails"));
+const Forecast = lazy(() => import("./Components/Forecast"));
 
 function App() {
-
 
   const [query, setQuery] = useState({ q: "london" });
   const [units, setUnits] = useState('metric');
@@ -51,10 +53,18 @@ function App() {
 
         {weather && (
           <div>
-            <TimeAndLocation weather={weather} />
-            <TemperatureDetails weather={weather} />
-            <Forecast title="hourly forecast" items={weather.hourly} />
-            <Forecast title="daily forecast" items={weather.daily} />
+            <Suspense fallback={<div>Loading Loaction...</div>}>
+              <TimeAndLocation weather={weather} />
+            </Suspense>
+
+            <Suspense fallback={<div>Loading Temperature...</div>}>
+              <TemperatureDetails weather={weather} />
+            </Suspense>
+
+            <Suspense fallback={<div>Loading Forecast</div>}>
+              <Forecast title="hourly forecast" items={weather.hourly} />
+              <Forecast title="daily forecast" items={weather.daily} />
+            </Suspense>
           </div>
         )}
 
